ae259ae9df15e6bd664e30b5ae077672e64602e5
kar2n <31457499+kar2n@users.noreply.github.com>
2024-11-08 13:33:32 -0300

    Update sample.py

--- sample.py
+++ sample.py
@@ -37,3 +37,11 @@ st.write("Text with write")
 
 # Writing python inbuilt function range()
 st.write(range(10))
+
+# import Image from pillow to open images
+from PIL import Image
+img = Image.open("streamlit.png")
+
+# display image using streamlit
+# width is used to set the width of an image
+st.image(img, width=200)

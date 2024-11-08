43feabeec52bab8c531dd92552b373be88799c28
kar2n <31457499+kar2n@users.noreply.github.com>
2024-11-08 13:37:23 -0300

    Update sample.py

--- sample.py
+++ sample.py
@@ -69,3 +69,11 @@ hobby = st.selectbox("Hobbies: ",
 
 # print the selected hobby
 st.write("Your hobby is: ", hobby)
+
+
+# Create a simple button that does nothing
+st.button("Click me for no reason")
+
+# Create a button, that when clicked, shows a text
+if(st.button("About")):
+    st.text("Welcome To GeeksForGeeks!!!")

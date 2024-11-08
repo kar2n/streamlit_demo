c00d202ea4e60e5f81402afded6c7a532d6e6478
kar2n <31457499+kar2n@users.noreply.github.com>
2024-11-08 13:36:31 -0300

    Update sample.py

--- sample.py
+++ sample.py
@@ -58,3 +58,14 @@ if (status == 'Male'):
     st.success("Male")
 else:
     st.success("Female")
+
+
+# Selection box
+
+# first argument takes the titleof the selectionbox
+# second argument takes options
+hobby = st.selectbox("Hobbies: ",
+                     ['Dancing', 'Reading', 'Sports'])
+
+# print the selected hobby
+st.write("Your hobby is: ", hobby)
